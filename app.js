49393c52950d78aaf7b7b9f3b25bf256e8fd2ed1
albertorodriguezdiaz <alberto@efectovirtual.com>
2021-10-31 20:04:03 -0500

    web ready

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const hbs = require('hbs');
+require('dotenv').config();
+
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
 
 
+// Handlebars
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(express.static('public'));
 
@@ -15,12 +20,18 @@ app.get('/', (req, res) =>{
 })
 
 app.get('/generic', (req, res) =>{
-    res.sendFile(__dirname + '/public/generic.html');
+    res.render('generic', {
+        nombre: 'Alberto Rodriguez',
+        titulo: 'Generic Page',
+    });
+})
+app.get('/elements', (req, res) =>{
+    res.render('elements', {
+        nombre: 'Alberto Rodriguez',
+        titulo: 'Elements Page',
+    });
 })
 
-app.get('/elements', (req, res) =>{
-    res.sendFile(__dirname + '/public/elements.html');
-})
 
 // app.get('*', (req, res) =>{
 //     res.sendFile(__dirname + '/public/404.html');
